f95b5dc0b706bce67ac6be8c6859354257a55cdb
hsuhsiaohsuan <hsuhsiaohsuan@gmail.com>
2014-04-01 15:18:33 +0800

    Initial commit

---
+++ src/idv/funnybrain/plurkchat/FunnyActivity.java
@@ -0,0 +1,15 @@
+package idv.funnybrain.plurkchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FunnyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
