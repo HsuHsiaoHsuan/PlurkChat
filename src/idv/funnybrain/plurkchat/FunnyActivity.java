e909b5c0c2a68b6ae521f664acf90a5237d07f3f
hsuhsiaohsuan <hsuhsiaohsuan@gmail.com>
2014-04-01 16:24:56 +0800

    init branch Freeman

--- src/idv/funnybrain/plurkchat/FunnyActivity.java
+++ src/idv/funnybrain/plurkchat/FunnyActivity.java
@@ -2,6 +2,10 @@ package idv.funnybrain.plurkchat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.PlurkApi;
+import org.scribe.model.Token;
+import org.scribe.oauth.OAuthService;
 
 public class FunnyActivity extends Activity {
     /**
@@ -11,5 +15,13 @@ public class FunnyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        OAuthService service = new ServiceBuilder().provider(PlurkApi.class)
+                                                   .apiKey("eOrMDFD8f5V2")
+                                                   .apiSecret("aiVo5OOdWJMl0vEEblPVNRmbG3EeIQh5")
+                                                   .build();
+        Token requestToken = service.getRequestToken();
+
+        String authUrl = service.getAuthorizationUrl(requestToken);
     }
 }
